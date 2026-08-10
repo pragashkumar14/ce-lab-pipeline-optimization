5655a1352e7ca05a0ae0cfd530f04283e543616f
Pragash Kumaravel <pragash_m@hotmail.co.uk>
2026-08-10 10:22:08 +0200

    fix: format outputs.tf per terraform fmt

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,7 +7,7 @@ output "bucket_arn" {
   description = "ARN of the logs bucket"
   value       = aws_s3_bucket.logs.arn
 }
- 
+
 output "bucket_encryption" {
   description = "Encryption algorithm used"
   value       = "AES256"
